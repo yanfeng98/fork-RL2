4cd2c701a819a12e01995c30ae7ccff899e8eec2
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-07-29 20:53:09 +0800

    remove filling zero adv

--- RL2/trainer/ppo.py
+++ RL2/trainer/ppo.py
@@ -8,8 +8,7 @@ from RL2.workers import Actor, Rollout, Critic
 from RL2.algs import (
     compute_approx_kl,
     compute_gae,
-    compute_reinforce_adv,
-    fill_zero_adv
+    compute_reinforce_adv
 )
 from RL2.utils.comm import initialize_global_process_group
 from RL2.utils.logging import time_logger
@@ -78,8 +77,6 @@ class PPOTrainer(Trainer):
                 self.config.data.responses_per_prompt,
                 self.config.adv.norm_var
             )
-        elif self.config.adv.estimator == "zeros":
-            fill_zero_adv(data_list)
         else: 
             raise NotImplementedError
             
