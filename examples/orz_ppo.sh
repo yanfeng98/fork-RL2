04768e8a47499a6b5b6f6c01147150de39a95d23
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-08-03 01:36:35 +0800

    update examples

--- examples/orz_ppo.sh
+++ examples/orz_ppo.sh
@@ -9,10 +9,10 @@ torchrun \
     actor.sp_size=2 \
     actor.max_length_per_device=8192 \
     actor.freeze_steps=4 \
-    actor.save_freq=32 \
     rollout.train_sampling_params.max_new_tokens=8192 \
     rollout.env_path=envs/orz.py \
     adv.estimator=gae \
     trainer.project=OpenReasonerZero \
     trainer.experiment_name=qwen2.5-7b-ppo \
-    trainer.test_freq=8
+    trainer.test_freq=8 \
+    trainer.save_freq=32
--- examples/orz_reinforce.sh
+++ examples/orz_reinforce.sh
@@ -8,10 +8,10 @@ torchrun \
     actor.model_name=Qwen/Qwen2.5-7B \
     actor.sp_size=2 \
     actor.max_length_per_device=8192 \
-    actor.save_freq=32 \
     rollout.train_sampling_params.max_new_tokens=8192 \
     rollout.env_path=envs/orz.py \
     adv.estimator=reinforce \
     trainer.project=OpenReasonerZero \
     trainer.experiment_name=qwen2.5-7b-reinforce \
-    trainer.test_freq=8
+    trainer.test_freq=8 \
+    trainer.save_freq=32
