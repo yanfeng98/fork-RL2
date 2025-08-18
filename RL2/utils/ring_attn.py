ba0df9b2dfdd54fe26a52db8cc567db762de086d
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-08-18 16:29:05 +0800

    add ring_attn_manager

--- RL2/utils/ring_attn.py
+++ RL2/utils/ring_attn.py
@@ -1,5 +1,6 @@
 from typing import Optional, Dict, Any
 import os
+import functools
 import torch
 import torch.distributed as dist
 from transformers.modeling_flash_attention_utils import (
@@ -69,15 +70,139 @@ def _flash_attention_forward(
 transformers.modeling_flash_attention_utils._flash_attention_forward = _flash_attention_forward
 ALL_ATTENTION_FUNCTIONS["flash_attention_2"] = flash_attention_forward
 
-def update_params_of_ring_attn(
-    cu_seqlens: torch.Tensor,
-    device_mesh: dist.DeviceMesh
-) -> None:
+def position_ids_to_cu_seqlens(position_ids):
 
-    max_seqlen = (cu_seqlens[1:] - cu_seqlens[:-1]).max().item()
+    indices = torch.arange(len(position_ids), dtype=torch.int32)
+    return torch.cat((
+        indices[position_ids == 0],
+        torch.tensor(position_ids.size(), dtype=torch.int32)
+    ))
+
+def ring_attn_preprocess(raw_minibatch, device_mesh):
+
+    cu_seqlens = position_ids_to_cu_seqlens(
+        raw_minibatch["position_ids"]
+    )
+    rank = device_mesh.get_local_rank()
+    double_size = 2 * device_mesh.size()
+    minibatch = {}
+    for k, v in raw_minibatch.items():
+        tensors = []
+        for start_idx, end_idx in zip(
+            cu_seqlens[:-1], cu_seqlens[1:]
+        ):
+            half_seqlen = (end_idx - start_idx) // double_size
+            tensor = v[start_idx:end_idx]
+            tensor = torch.cat((
+                tensor[rank * half_seqlen:(rank + 1) * half_seqlen],
+                tensor[(double_size - rank - 1) * half_seqlen: (double_size - rank) * half_seqlen]
+            ))
+            tensors.append(tensor)
+        minibatch[k] = torch.cat(tensors)
+    cu_seqlens /= device_mesh.size()
+    return minibatch, cu_seqlens
+
+def pad_tokens_and_unsqueeze(minibatch, cu_seqlens, multiple_of):
+
+    if len(minibatch["states"]) % multiple_of != 0:
+        pad_tokens = multiple_of - len(minibatch["states"]) % multiple_of
+        minibatch = {
+            k: torch.cat((
+                v, torch.zeros((pad_tokens), dtype=v.dtype)
+            ))
+            for k, v in minibatch.items()
+        }
+        cu_seqlens = torch.cat((
+            cu_seqlens,
+            torch.tensor(
+                minibatch["position_ids"].size(),
+                dtype=torch.int32
+            )
+        ))
+    else:
+        pad_tokens = 0
+        
+    minibatch = {
+        k: v.unsqueeze(0).to(torch.cuda.current_device())
+        for k, v in minibatch.items()
+    }
+    cu_seqlens = cu_seqlens.to(torch.cuda.current_device())
+
+    return minibatch, cu_seqlens, pad_tokens
+
+def squeeze_and_remove_pad_tokens(output, pad_tokens):
+
+    if isinstance(output, tuple):
+        return tuple(
+            squeeze_and_remove_pad_tokens(tensor, pad_tokens)
+            for tensor in output
+        )
     
-    DATA_PARAMS.update({
-        "group": device_mesh.get_group(),
-        "cu_seqlens": cu_seqlens,
-        "max_seqlen": max_seqlen
-    })
+    output = output.squeeze(0)
+    return output[:-pad_tokens] if pad_tokens > 0 else output
+        
+def gather_tensor_along_sp_dim(tensor, device_mesh):
+
+    tensors = [
+        torch.zeros_like(tensor)
+        for _ in range(device_mesh.size())
+    ]
+    dist.all_gather(
+        tensors,
+        tensor,
+        group=device_mesh.get_group()
+    )
+    tensors[device_mesh.get_local_rank()] = tensor # necessary to retain grad
+    half_seqlen = len(tensor) // 2
+    inorder_tensors = [
+        tensor[:half_seqlen] for tensor in tensors
+    ]
+    reversed_tensors = [
+        tensor[half_seqlen:] for tensor in tensors
+    ]
+    return torch.cat(inorder_tensors + reversed_tensors[::-1])
+
+def ring_attn_postprocess(output, cu_seqlens, device_mesh):
+
+    if isinstance(output, tuple):
+        return tuple(
+            ring_attn_postprocess(tensor, cu_seqlens, device_mesh)
+            for tensor in output
+        )
+
+    return torch.cat((
+        gather_tensor_along_sp_dim(
+            output[start_idx:end_idx], device_mesh
+        )
+        for start_idx, end_idx in zip(
+            cu_seqlens[:-1], cu_seqlens[1:]
+        )
+    ))
+
+def ring_attn_manager(func):
+
+    @functools.wraps(func)
+    def forward_with_ring_attn(self, minibatch, *args, **kwargs):
+
+        minibatch, cu_seqlens = ring_attn_preprocess(
+            minibatch, self.device_mesh["sp"]
+        )
+        minibatch, cu_seqlens, pad_tokens = pad_tokens_and_unsqueeze(
+            minibatch, cu_seqlens, self.device_mesh["tp"].size()
+        )
+
+        max_seqlen = (cu_seqlens[1:] - cu_seqlens[:-1]).max().item()
+        DATA_PARAMS.update({
+            "group": self.device_mesh["sp"].get_group(),
+            "cu_seqlens": cu_seqlens,
+            "max_seqlen": max_seqlen
+        })
+
+        output = func(self, minibatch, *args, **kwargs)
+
+        output = squeeze_and_remove_pad_tokens(output, pad_tokens)
+        return ring_attn_postprocess(
+            output, cu_seqlens, self.device_mesh["sp"]
+        )
+    
+    return forward_with_ring_attn
