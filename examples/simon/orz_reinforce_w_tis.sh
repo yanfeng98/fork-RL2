48584f539d952ba55dfead642d867b900df16cbc
simon <simon011130@gmail.com>
2025-08-26 14:05:57 -0700

    add remote examples

---
+++ examples/simon/orz_reinforce_w_tis.sh
@@ -0,0 +1,18 @@
+torchrun \
+    --nproc_per_node=8 \
+    -m RL2.trainer.ppo \
+    train_data.path=Chenmien/OpenReasonerZero \
+    train_data.prompts_per_rollout=128 \
+    train_data.responses_per_prompt=64 \
+    test_data.path=Chenmien/OlympiadBench \
+    actor.model_name=Qwen/Qwen2.5-3B \
+    actor.sp_size=2 \
+    actor.max_length_per_device=8192 \
+    actor.tis_coef=2.0 \
+    rollout.train_sampling_params.max_new_tokens=8192 \
+    rollout.env_path=envs/orz.py \
+    adv.estimator=reinforce \
+    trainer.project=OpenReasonerZero \
+    trainer.experiment_name=qwen2.5-3b-reinforce \
+    trainer.test_freq=8 \
+    trainer.save_freq=32
