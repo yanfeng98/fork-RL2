0aff42446cb1b239fcc6f82ac086e1af33b9ea4a
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-09-02 00:48:04 +0800

    adapt algorithms to tensor_dict

--- RL2/utils/algorithms.py
+++ RL2/utils/algorithms.py
@@ -19,19 +19,24 @@ def compute_approx_kl(
     else:
         raise NotImplementedError
 
-def compute_gae(tensor_dicts, gamma, lamda):
+def compute_gae(tensor_dict, gamma, lamda):
 
     # extract rewards and values of action tokens
-    rewards, values, action_mask = [], [], []
-    for td in tensor_dicts:
-        indices = torch.where(td["action_mask"])[0]
-        rewards.append(td["rewards"][indices])
-        values.append(td["values"][indices])
-        action_mask.append(td["action_mask"][indices])
+    rewards, values, action_masks = [], [], []
+    for reward, value, action_mask in zip(
+        tensor_dict["rewards"],
+        tensor_dict["values"],
+        tensor_dict["action_mask"]
+    ):
+        indices = torch.where(action_mask)[0]
+        rewards.append(reward[indices])
+        values.append(value[indices])
+        action_masks.append(action_mask[indices])
+
     # pad to identical length for efficient computation
     rewards = pad_sequence(rewards, True)
     values = pad_sequence(values, True)
-    action_mask = pad_sequence(action_mask, True)
+    action_masks = pad_sequence(action_masks, True)
     
     # \delta_t = r_t + \gamma * V(s_{t+1}) - V(s_t)
     next_values = torch.cat((
@@ -48,28 +53,32 @@ def compute_gae(tensor_dicts, gamma, lamda):
     gaes = torch.stack(reversed_gaes[::-1], -1)
     returns = gaes + values
 
-    action_gaes = gaes[torch.where(action_mask)]
-    gaes = (gaes - action_gaes.mean()) * action_mask / (
+    action_gaes = gaes[torch.where(action_masks)]
+    gaes = (gaes - action_gaes.mean()) * action_masks / (
         action_gaes.std() + torch.finfo(gaes.dtype).eps
     )
 
-    for td, gae, ret in zip(tensor_dicts, gaes, returns):
-        td["advantages"] = torch.zeros_like(td["rewards"])
-        td["returns"] = torch.zeros_like(td["rewards"])
-        indices = torch.where(td["action_mask"])[0]
-        td["advantages"][indices] = gae[:len(indices)]
-        td["returns"][indices] = ret[:len(indices)]
+    tensor_dict["advantages"] = torch.zeros_like(tensor_dict["rewards"])
+    tensor_dict["returns"] = torch.zeros_like(tensor_dict["rewards"])
+    for advantage, ret, action_mask, gae, action_ret in zip(
+        tensor_dict["advantages"],
+        tensor_dict["returns"],
+        tensor_dict["action_mask"],
+        gaes,
+        returns
+    ):
+        indices = torch.where(action_mask)[0]
+        advantage[indices] = gae[:len(indices)]
+        ret[indices] = action_ret[:len(indices)]
 
 def compute_reinforce_adv(
-    tensor_dicts,
+    tensor_dict,
     responses_per_prompt,
     global_norm: bool,
     norm_var: bool
 ):
     
-    rewards = torch.FloatTensor(
-        [td["rewards"].sum() for td in tensor_dicts]
-    ).view(-1, responses_per_prompt)
+    rewards = tensor_dict["rewards"].sum(-1).view(-1, responses_per_prompt)
 
     if global_norm:
         baseline = rewards.mean()
@@ -84,5 +93,4 @@ def compute_reinforce_adv(
             std + torch.finfo(advantages.dtype).eps
         )
 
-    for td, advantage in zip(tensor_dicts, advantages.flatten()):
-        td["advantages"] = advantage * td["action_mask"]
+    tensor_dict["advantages"] = advantages.view(-1, 1) * tensor_dict["action_mask"]
