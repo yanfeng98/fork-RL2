22621e480db0f2ef3cd13f1bafe983f01974d06d
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-09-11 22:27:41 +0800

    update GEM example

--- examples/gem_reinforce.sh
+++ examples/gem_reinforce.sh
@@ -4,12 +4,11 @@ torchrun \
     train_data.prompts_per_rollout=64 \
     actor.model_name=Qwen/Qwen3-1.7B-Base \
     actor.max_length_per_device=8192 \
-    rollout.train_sampling_params.max_new_tokens=4096 \
+    rollout.train_sampling_params.max_new_tokens=1024 \
     rollout.env_path=envs/gem.py \
-    rollout.max_turns=16 \
     adv.global_norm=true \
     adv.norm_var=true \
     trainer.project=GEM \
-    trainer.experiment_name=language-games_qwen3-1.7b_reinforce \
+    trainer.experiment_name=letter-counting_qwen3-1.7b_reinforce \
     trainer.n_epochs=512 \
     trainer.save_freq=64
